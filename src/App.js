f773c7bd5dfc7815906e2b50eea98c9e59ff09ac
hhimanshu <harit.himanshu@yahoo.in>
2017-04-18 13:56:27 +1200

    - Added more menu items - Added max-width for large screen in media queries - Style Row under ListItem

--- src/App.js
+++ src/App.js
@@ -1,15 +1,24 @@
 import React from "react";
 import "./App.css";
+import {Row} from "react-flexbox-grid";
 import MuiThemeProvider from "material-ui/styles/MuiThemeProvider";
-import AppBar from "material-ui/AppBar";
-import MenuItem from "material-ui/MenuItem";
-import IconMenu from "material-ui/IconMenu/IconMenu";
-import IconButton from "material-ui/IconButton/IconButton";
+import {AppBar, MenuItem, IconMenu, IconButton} from "material-ui";
 import MoreVertIcon from "material-ui/svg-icons/navigation/more-vert";
 import SpicyMenu from "./menus/SpicyMenu";
+
 import breadSandwich from "./images/bread-food-salad-sandwich.jpg";
 import spaghettiPasta from "./images/food-dinner-pasta-spaghetti.jpg";
-import {Row} from "react-flexbox-grid";
+import healthySalad from "./images/food-salad-healthy-lunch.jpg";
+import meatMushroom from "./images/meat-vegetables-gemuesepiess-mushrooms-111131.jpeg";
+import muffin from "./images/muffin.jpg";
+import tomYumSoup from "./images/pexels-photo-48726.jpeg";
+import burgerFries from "./images/pexels-photo-70497.jpeg";
+import stirFry from "./images/pexels-photo-76093.jpeg";
+import spinachSalad from "./images/pexels-photo-169743.jpeg";
+import kaleSalad from "./images/salad-healthy-diet-spinach.jpg";
+import salmonCake from "./images/salmon-dish-food-meal-46239.jpeg";
+import pizza from "./images/vegetables-italian-pizza-restaurant.jpg";
+
 
 const foodItems = [
     {
@@ -22,6 +31,51 @@ const foodItems = [
         "name": "Spaghetti Pasta",
         "image": spaghettiPasta,
         "price": "$11.23"
+    },
+    {
+        "name": "Healthy Salad",
+        "image": healthySalad,
+        "price": "$12.99"
+    },
+    {
+        "name": "Meat Mushroom",
+        "image": meatMushroom,
+        "price": "$7.99"
+    },
+    {
+        "name": "Muffin",
+        "image": muffin,
+        "price": "$3.99"
+    },
+    {
+        "name": "Tom Yum Soup",
+        "image": tomYumSoup,
+        "price": "$5.55"
+    },
+    {
+        "name": "Burger Fries",
+        "image": burgerFries,
+        "price": "$11.10"
+    },
+    {
+        "name": "Stir Fry",
+        "image": stirFry,
+        "price": "$7.45"
+    },
+    {
+        "name": "Spinach Salad",
+        "image": spinachSalad,
+        "price": "$6.55"
+    },
+    {
+        "name": "Salmon Cake",
+        "image": salmonCake,
+        "price": "$6.10"
+    },
+    {
+        "name": "Italian Pizza",
+        "image": pizza,
+        "price": "$11.99"
     }
 ];
 
@@ -38,15 +92,10 @@ const AppMenu = () => {
     );
 };
 
-
-const HeaderStyle = {
-    'marginBottom': '3%'
-};
-
 const App = () => (
     <MuiThemeProvider>
         <div>
-            <div style={HeaderStyle}>
+            <div>
                 <Row around="lg">
                     <AppBar
                         title="SpicyVeggie"
@@ -55,10 +104,11 @@ const App = () => (
                     />
                 </Row>
             </div>
-            <SpicyMenu foodItems={foodItems}/>
+            <div id="content-root">
+                <SpicyMenu foodItems={foodItems}/>
+            </div>
         </div>
     </MuiThemeProvider>
 );
-
 
 export default App;
--- src/menus/SpicyMenu.js
+++ src/menus/SpicyMenu.js
@@ -1,30 +1,30 @@
 import React from "react";
-import Avatar from 'material-ui/Avatar';
-import List from "material-ui/List";
+import {Avatar, List, ListItem} from 'material-ui';
 import {Grid, Row, Col} from 'react-flexbox-grid';
 
 // https://www.pexels.com/photo-license/
 // All photos on Pexels are free for any personal and commercial purpose.
 
 const SpicyMenu = (props) => (
-
     <List>
         {props.foodItems.map(foodItem => <SpicyMenuItem key={foodItem.name} {...foodItem}/>)}
     </List>
 );
 
-const style = {
+const RowItemStyle = {
     alignItems: "center"
 };
 
 const SpicyMenuItem = (props) => (
-    <Grid fluid>
-        <Row center="lg" style={style}>
-            <Col xs={3} sm={3} lg={2}><Avatar src={props.image}/></Col>
-            <Col xs={6} sm={6} lg={4}>{props.name}</Col>
-            <Col xs={3} sm={3} lg={2}>{props.price}</Col>
-        </Row>
-    </Grid>
+    <ListItem>
+        <Grid fluid>
+            <Row center="lg" style={RowItemStyle}>
+                <Col xs={3} sm={3} lg={2}><Avatar src={props.image}/></Col>
+                <Col xs={6} sm={6} lg={4}>{props.name}</Col>
+                <Col xs={3} sm={3} lg={2}>{props.price}</Col>
+            </Row>
+        </Grid>
+    </ListItem>
 );
 
 export default SpicyMenu;
